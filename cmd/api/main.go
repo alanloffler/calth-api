20ebdd4c633cef3a12bffe1371ff0e1ab778115b
alanloffler <alanmatiasloffler@gmail.com>
2026-01-29 20:41:44 -0300

    Gin server

---
+++ cmd/api/main.go
@@ -0,0 +1,16 @@
+package main
+
+import "github.com/gin-gonic/gin"
+
+func main() {
+	var router *gin.Engine = gin.Default()
+	router.SetTrustedProxies(nil)
+	router.GET("/", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "Calth API running",
+			"status":  "success",
+		})
+	})
+
+	router.Run(":3000")
+}
