37f142bd31a205a31ccedb8dad3230d0ce0b60b9
alanloffler <alanmatiasloffler@gmail.com>
2026-02-12 12:44:55 -0300

    feat(events): select constants

---
+++ src/events/constants/event-select.constant.ts
@@ -0,0 +1,19 @@
+export const EVENT_USER_SELECT = [
+  "user.email",
+  "user.firstName",
+  "user.ic",
+  "user.id",
+  "user.lastName",
+  "user.phoneNumber",
+];
+
+export const EVENT_ROLE_SELECT = ["userRole.name", "userRole.value"];
+
+export const EVENT_PROF_SELECT = [
+  "professional.firstName",
+  "professional.ic",
+  "professional.id",
+  "professional.lastName",
+];
+
+export const EVENT_PROF_PROFILE_SELECT = ["professionalProfile.professionalPrefix"];
