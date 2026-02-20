02d43fc1e4443290d1557d450089851c35b7bf3e
alanloffler <alanmatiasloffler@gmail.com>
2026-02-20 18:01:40 -0300

    refactor(decorators): update access token var name

--- src/common/decorators/business-id.decorator.ts
+++ src/common/decorators/business-id.decorator.ts
@@ -5,7 +5,7 @@ import type { IPayload } from "@auth/interfaces/payload.interface";
 
 export const BusinessId = createParamDecorator((_: unknown, ctx: ExecutionContext): string | undefined => {
   const request = ctx.switchToHttp().getRequest();
-  const token = request.cookies?.accessToken;
+  const token = request.cookies?.access_token;
 
   if (!token) return undefined;
 
