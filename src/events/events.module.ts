585c226217352eb4c09cff7932d4a3f764e5fca6
alanloffler <alanmatiasloffler@gmail.com>
2026-03-29 18:09:46 -0300

    feat(events): import professional profile module

--- src/events/events.module.ts
+++ src/events/events.module.ts
@@ -6,11 +6,17 @@ import { Event } from "@events/entities/event.entity";
 import { EventsController } from "@events/events.controller";
 import { EventsService } from "@events/events.service";
 import { Permission } from "@permissions/entities/permission.entity";
+import { ProfessionalProfileModule } from "@professional-profile/professional-profile.module";
 import { Role } from "@roles/entities/role.entity";
 import { UsersModule } from "@users/users.module";
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Event, Permission, Role]), BusinessModule, UsersModule],
+  imports: [
+    TypeOrmModule.forFeature([Event, Permission, Role]),
+    BusinessModule,
+    ProfessionalProfileModule,
+    UsersModule,
+  ],
   controllers: [EventsController],
   providers: [EventsService],
 })
