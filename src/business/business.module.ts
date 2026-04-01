a8beb96df129d1478d987da4fc16581498df97c5
alanloffler <alanmatiasloffler@gmail.com>
2026-04-01 15:18:14 -0300

    feat(business): import UsersModule and use case for transaction

--- src/business/business.module.ts
+++ src/business/business.module.ts
@@ -4,13 +4,15 @@ import { TypeOrmModule } from "@nestjs/typeorm";
 import { Business } from "@business/entities/business.entity";
 import { BusinessController } from "@business/business.controller";
 import { BusinessService } from "@business/business.service";
+import { CreateBusinessWithAdminUseCase } from "@business/use-cases/create-business-with-admin.use-case";
 import { Permission } from "@permissions/entities/permission.entity";
 import { Role } from "@roles/entities/role.entity";
+import { UsersModule } from "@users/users.module";
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Business, Permission, Role])],
+  imports: [TypeOrmModule.forFeature([Business, Permission, Role]), UsersModule],
   controllers: [BusinessController],
-  providers: [BusinessService],
+  providers: [BusinessService, CreateBusinessWithAdminUseCase],
   exports: [BusinessService],
 })
 export class BusinessModule {}
