7025b46471093df6a4b67a8b706f664049ee7c8a
alanloffler <alanmatiasloffler@gmail.com>
2026-04-12 16:58:13 -0300

    feat(notifications): email service

---
+++ src/notifications/email/email.service.ts
@@ -0,0 +1,22 @@
+import sgMail from "@sendgrid/mail";
+
+export class EmailService {
+  constructor() {
+    const apiKey = process.env.SENDGRID_API_KEY;
+
+    if (!apiKey) {
+      throw new Error("SENDGRID_API_KEY is not defined");
+    }
+
+    sgMail.setApiKey(apiKey);
+  }
+
+  async sendClinicCreatedEmail(to: string, clinicName: string) {
+    await sgMail.send({
+      to,
+      from: "no-reply@calth.com",
+      subject: "Clínica creada",
+      html: `<h1>Tu clínica ${clinicName} fue creada</h1>`,
+    });
+  }
+}
