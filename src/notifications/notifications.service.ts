3330be41d4ebdf027e4d3919ec05e809685190d8
alanloffler <alanmatiasloffler@gmail.com>
2026-04-12 19:42:48 -0300

    feat(notifications): inject bullmq queue

--- src/notifications/notifications.service.ts
+++ src/notifications/notifications.service.ts
@@ -1,13 +1,15 @@
+import { InjectQueue } from "@nestjs/bullmq";
 import { Injectable } from "@nestjs/common";
 import { OnEvent } from "@nestjs/event-emitter";
-
-import { notificationsQueue } from "@notifications/queue";
+import { Queue } from "bullmq";
 
 @Injectable()
 export class NotificationsService {
+  constructor(@InjectQueue("notifications") private readonly queue: Queue) {}
+
   @OnEvent("clinic.created")
   async handleClinicCreatedEvent(payload: { email: string; clinicName: string }) {
-    await notificationsQueue.add("send-email", {
+    await this.queue.add("send-email", {
       type: "clinic-created",
       ...payload,
     });
