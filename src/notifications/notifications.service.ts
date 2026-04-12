48b9a8812cc5b71a55093e9f158aaa2c8e0c3fac
alanloffler <alanmatiasloffler@gmail.com>
2026-04-12 17:54:17 -0300

    refactor(notifications): remove logger

--- src/notifications/notifications.service.ts
+++ src/notifications/notifications.service.ts
@@ -1,18 +1,15 @@
-import { Injectable, Logger } from "@nestjs/common";
+import { Injectable } from "@nestjs/common";
 import { OnEvent } from "@nestjs/event-emitter";
-import { notificationsQueue } from "./queue";
+
+import { notificationsQueue } from "@notifications/queue";
 
 @Injectable()
 export class NotificationsService {
-  private readonly logger = new Logger(NotificationsService.name);
-
   @OnEvent("clinic.created")
   async handleClinicCreatedEvent(payload: { email: string; clinicName: string }) {
-    this.logger.log(`clinic.created event received for ${payload.email}`);
-    const job = await notificationsQueue.add("send-email", {
+    await notificationsQueue.add("send-email", {
       type: "clinic-created",
       ...payload,
     });
-    this.logger.log(`Job ${job.id} added to queue`);
   }
 }
