42f2d9da145d99ac9756e08788eb3779c5fb6a23
alanloffler <alanmatiasloffler@gmail.com>
2026-04-13 16:05:41 -0300

    feat(notifications): render template for html

--- src/notifications/email/email.service.ts
+++ src/notifications/email/email.service.ts
@@ -1,6 +1,8 @@
 import sgMail from "@sendgrid/mail";
 import { ConfigService } from "@nestjs/config";
 import { Injectable } from "@nestjs/common";
+import { join } from "path";
+import { readFileSync } from "fs";
 
 @Injectable()
 export class EmailService {
@@ -13,12 +15,21 @@ export class EmailService {
     sgMail.setApiKey(apiKey);
   }
 
-  async sendBusinessCreatedEmail(to: string, companyName: string) {
+  async sendBusinessCreatedEmail(to: string, companyName: string, companyLink: string) {
     await sgMail.send({
       to,
       from: this.fromEmail,
       subject: "Calth - Alta de negocio",
-      html: `<h1>Tu negocio ${companyName} fue creado</h1>`,
+      html: this.renderTemplate("business-created", { companyName, companyLink }),
     });
+  }
+
+  private renderTemplate(name: string, data: Record<string, string>): string {
+    const filePath = join(__dirname, "templates", `${name}.html`);
+    let html = readFileSync(filePath, "utf-8");
+    for (const [key, value] of Object.entries(data)) {
+      html = html.replaceAll(`{{${key}}}`, value);
+    }
+    return html;
   }
 }
