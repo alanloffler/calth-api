e118c0cea0b82b7c5023aff91d562a4017770cc2
alanloffler <alanmatiasloffler@gmail.com>
2026-04-22 18:18:33 -0300

    feat(blocked-days): role import for permissions

--- src/blocked-days/blocked-days.module.ts
+++ src/blocked-days/blocked-days.module.ts
@@ -4,9 +4,10 @@ import { TypeOrmModule } from "@nestjs/typeorm";
 import { BlockedDay } from "@blocked-days/entities/blocked-day.entity";
 import { BlockedDaysController } from "@blocked-days/blocked-days.controller";
 import { BlockedDaysService } from "@blocked-days/blocked-days.service";
+import { Role } from "@roles/entities/role.entity";
 
 @Module({
-  imports: [TypeOrmModule.forFeature([BlockedDay])],
+  imports: [TypeOrmModule.forFeature([BlockedDay, Role])],
   controllers: [BlockedDaysController],
   providers: [BlockedDaysService],
 })
