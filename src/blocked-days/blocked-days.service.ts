a73ecfac47d45fd64974d44abf6ead57ff4ebfca
alanloffler <alanmatiasloffler@gmail.com>
2026-04-22 17:41:42 -0300

    feat(blocked-days): create service

--- src/blocked-days/blocked-days.service.ts
+++ src/blocked-days/blocked-days.service.ts
@@ -1,18 +1,24 @@
 import { InjectRepository } from "@nestjs/typeorm";
-import { Injectable } from "@nestjs/common";
+import { HttpException, HttpStatus, Injectable } from "@nestjs/common";
 import { Repository } from "typeorm";
 
 import { BlockedDay } from "@blocked-days/entities/blocked-day.entity";
 import { CreateBlockedDayDto } from "@blocked-days/dto/create-blocked-day.dto";
 import { UpdateBlockedDayDto } from "@blocked-days/dto/update-blocked-day.dto";
+import { ApiResponse } from "@common/helpers/api-response.helper";
 
 @Injectable()
 export class BlockedDaysService {
   constructor(@InjectRepository(BlockedDay) private readonly blockedDayRepository: Repository<BlockedDay>) {}
 
-  async create(businessId: string, createBlockedDayDto: CreateBlockedDayDto) {
-    console.log(createBlockedDayDto);
-    return `This action adds a new blockedDay to business #${businessId}`;
+  async create(businessId: string, createBlockedDayDto: CreateBlockedDayDto): Promise<ApiResponse<BlockedDay>> {
+    const blockedDay = this.blockedDayRepository.create({ ...createBlockedDayDto, businessId });
+    const savedBlockedDay = await this.blockedDayRepository.save(blockedDay);
+
+    if (!savedBlockedDay) throw new HttpException("Error al crear día bloqueado", HttpStatus.INTERNAL_SERVER_ERROR);
+    // TODO: throw custom exception on duplicated blocked day
+
+    return ApiResponse.created<BlockedDay>("Día bloqueado creado", savedBlockedDay);
   }
 
   async findOne(businessId: string, id: string) {
