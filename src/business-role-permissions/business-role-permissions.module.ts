c297268b43f4758603f5664e6c7a615a55cb2950
alanloffler <alanmatiasloffler@gmail.com>
2026-05-02 16:05:17 -0300

    feat(brp): import Role entity for permission guard

--- src/business-role-permissions/business-role-permissions.module.ts
+++ src/business-role-permissions/business-role-permissions.module.ts
@@ -4,9 +4,10 @@ import { TypeOrmModule } from "@nestjs/typeorm";
 import { BusinessRolePermission } from "@business-role-permissions/entities/business-role-permission.entity";
 import { BusinessRolePermissionsController } from "@business-role-permissions/business-role-permissions.controller";
 import { BusinessRolePermissionsService } from "@business-role-permissions/business-role-permissions.service";
+import { Role } from "@roles/entities/role.entity";
 
 @Module({
-  imports: [TypeOrmModule.forFeature([BusinessRolePermission])],
+  imports: [TypeOrmModule.forFeature([BusinessRolePermission, Role])],
   controllers: [BusinessRolePermissionsController],
   providers: [BusinessRolePermissionsService],
 })
