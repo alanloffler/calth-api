3c87de4e7bacbd28abe7725fc50745c3c05c7264
alanloffler <alanmatiasloffler@gmail.com>
2026-05-02 15:32:29 -0300

    delete(brp): deprecated update dto

--- src/business-role-permissions/dto/update-business-role-permission.dto.ts
+++
@@ -1,5 +0,0 @@
-import { PartialType } from "@nestjs/mapped-types";
-
-import { CreateBusinessRolePermissionDto } from "@business-role-permissions/dto/create-business-role-permission.dto";
-
-export class UpdateBusinessRolePermissionDto extends PartialType(CreateBusinessRolePermissionDto) {}
