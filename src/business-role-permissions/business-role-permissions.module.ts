7479cb076c90ff9bb49d2f0b8a7015efeeba8a1e
alanloffler <alanmatiasloffler@gmail.com>
2026-05-02 16:50:01 -0300

    feat(brp): import permission module & export brp service

--- src/business-role-permissions/business-role-permissions.module.ts
+++ src/business-role-permissions/business-role-permissions.module.ts
@@ -4,11 +4,13 @@ import { TypeOrmModule } from "@nestjs/typeorm";
 import { BusinessRolePermission } from "@business-role-permissions/entities/business-role-permission.entity";
 import { BusinessRolePermissionsController } from "@business-role-permissions/business-role-permissions.controller";
 import { BusinessRolePermissionsService } from "@business-role-permissions/business-role-permissions.service";
+import { PermissionsModule } from "@permissions/permissions.module";
 import { Role } from "@roles/entities/role.entity";
 
 @Module({
-  imports: [TypeOrmModule.forFeature([BusinessRolePermission, Role])],
+  imports: [TypeOrmModule.forFeature([BusinessRolePermission, Role]), PermissionsModule],
   controllers: [BusinessRolePermissionsController],
   providers: [BusinessRolePermissionsService],
+  exports: [BusinessRolePermissionsService],
 })
 export class BusinessRolePermissionsModule {}
