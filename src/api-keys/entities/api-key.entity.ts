7bb55c7ae3bd4e25b44da4bf041f4ed1f0988354
alanloffler <alanmatiasloffler@gmail.com>
2026-05-04 17:35:01 -0300

    feat(api-keys): entity

---
+++ src/api-keys/entities/api-key.entity.ts
@@ -0,0 +1,24 @@
+import { Column, CreateDateColumn, Entity, Index, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+
+@Entity()
+@Index("idx_api_keys_business_id", ["businessId"])
+@Index("uq_api_keys_business_name", ["businessId", "name"], { unique: true })
+export class ApiKey {
+  @PrimaryGeneratedColumn("uuid")
+  id: string;
+
+  @Column({ type: "uuid", name: "business_id", nullable: false })
+  businessId: string;
+
+  @Column({ type: "varchar", length: 50, nullable: false })
+  name: string;
+
+  @Column({ type: "text", nullable: false })
+  key: string;
+
+  @CreateDateColumn({ type: "timestamptz", name: "created_at" })
+  createdAt: Date;
+
+  @UpdateDateColumn({ type: "timestamptz", name: "updated_at" })
+  updatedAt: Date;
+}
