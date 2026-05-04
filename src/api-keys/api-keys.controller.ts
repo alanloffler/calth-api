5e4ec10e6f4cee85f557c7d76171237c6379078e
alanloffler <alanmatiasloffler@gmail.com>
2026-05-04 18:35:57 -0300

    feat(api-keys): remove controller

--- src/api-keys/api-keys.controller.ts
+++ src/api-keys/api-keys.controller.ts
@@ -29,7 +29,7 @@ export class ApiKeysController {
   }
 
   @Delete(":id")
-  remove(@Param("id") id: string) {
-    return this.apiKeysService.remove(+id);
+  remove(@BusinessId(ParseUUIDPipe) businessId: string, @Param("id") id: string) {
+    return this.apiKeysService.remove(businessId, id);
   }
 }
