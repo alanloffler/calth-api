2b04d1108398a5eb0ff899f99058298d8301e585
alanloffler <alanmatiasloffler@gmail.com>
2026-05-05 12:02:45 -0300

    feat(api-keys): inject encryption provider

--- src/api-keys/api-keys.module.ts
+++ src/api-keys/api-keys.module.ts
@@ -4,10 +4,11 @@ import { TypeOrmModule } from "@nestjs/typeorm";
 import { ApiKey } from "@api-keys/entities/api-key.entity";
 import { ApiKeysController } from "@api-keys/api-keys.controller";
 import { ApiKeysService } from "@api-keys/api-keys.service";
+import { EncryptionService } from "@common/services/encryption.service";
 
 @Module({
   imports: [TypeOrmModule.forFeature([ApiKey])],
   controllers: [ApiKeysController],
-  providers: [ApiKeysService],
+  providers: [ApiKeysService, EncryptionService],
 })
 export class ApiKeysModule {}
